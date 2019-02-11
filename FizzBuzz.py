e5ba254aa55dbc862d83d0807e0c0014cd0d3ad1
Tai Dunn <39572984+taidunn@users.noreply.github.com>
2019-02-11 13:25:34 -0500

    Update FizzBuzz.py
    Left out a part of the programming problem by accident so I have updated to the newest version.

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -9,7 +9,13 @@
 # For numbers which are multiples of both three and five print "FizzBuzz".
 
 for value in range(0,101):
-    if value % 3 == 0:
+    if (value % 5 == 0 and value % 3 == 0):
+        print('FizzBuzz')
+    elif value % 3 == 0:
         print('Fizz')
     elif value % 5 == 0:
         print('Buzz')
+# Only take out these '#' if you want it to print out the number for ones that
+# are none of the above!
+#    else:
+#        print(value)
